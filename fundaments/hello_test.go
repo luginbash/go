640a51ab86ed6ca7dfe9faea9ddb328ccfb36ed0
Luginbash <725033+luginbash@users.noreply.github.com>
2019-12-24 11:00:24 +0800

    Expect a comma after Hello

--- fundaments/hello_test.go
+++ fundaments/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
   got := Hello("Name")
-  want := "Hello Name"
+  want := "Hello, Name"
 
   if got != want {
     t.Errorf("got %q want %q", got, want)
