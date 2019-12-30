b287e4b118a4bd664cf2c545b94048f2d6cb4282
Luginbash <725033+luginbash@users.noreply.github.com>
2019-12-30 16:52:41 +0800

    Finish the array and slices chapter

--- fundaments/arr/sum.go
+++ fundaments/arr/sum.go
@@ -1,13 +1,31 @@
 package main
 
-
 func Sum(numbers []int) int {
-  sum := 0
-  for _, number := range numbers {
-    sum += number
-  }
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
 
-  return sum
+	return sum
 }
 
+func SumAll(nums2Sum ...[]int) []int {
+	var sums []int
+	for _, numbers := range nums2Sum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
 
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
