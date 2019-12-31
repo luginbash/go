2cef48b5b4231903a98ecf32656c005c9257968f
Luginbash <725033+luginbash@users.noreply.github.com>
2019-12-31 18:24:59 +0800

    Add wallet prototype

---
+++ fundaments/wallet/wallet_test.go
@@ -0,0 +1,46 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+  t.Run("Deposit", func(t *testing.T) {
+    wallet := Wallet{}
+    wallet.Deposit(Bitcoin(10))
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("Withdraw with funds", func(t *testing.T) {
+    wallet := Wallet{Bitcoin(20)}
+    wallet.Withdraw(Bitcoin(10))
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("Withdraw insufficient funds", func(t *testing.T) {
+    wallet := Wallet{Bitcoin(20)}
+    err := wallet.Withdraw(Bitcoin(100))
+
+    assertBalance(t, wallet, Bitcoin(20))
+    assertError(t, err, ErrInsufficientFunds)
+  })
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+  t.Helper()
+  got := wallet.Balance()
+
+  if got != want {
+    t.Errorf("got %q want %q", got, want)
+  }
+}
+
+func assertError(t *testing.T, got error, want error) {
+  t.Helper()
+  if got == nil {
+    t.Fatal("didn't get an error but wanted one")
+  }
+
+  if got != want {
+    t.Errorf("got %q, want %q", got, want)
+  }
+}
